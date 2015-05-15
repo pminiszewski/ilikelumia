6d5bebdf9583dc5d87f4d5d5078abd6de2942999
Pawel Miniszewski <venetian5@gmail.com>
2015-05-16 00:55:23 +0200

    - dodane zrodlo jedzenia

--- dolanburger/Assets/scripts/Bucket.cs
+++ dolanburger/Assets/scripts/Bucket.cs
@@ -1,19 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Bucket : MonoBehaviour 
 {
-
+	public Item ItemPrefab;
 	public FoodType BucketType;
+
 
 
 	public void OnSpawnItem()
 	{
-		GameObject go = new GameObject();
-
-		Item i = go.AddComponent<Item>();
-		i.FType = BucketType;
+		GameObject go = Instantiate<GameObject>(ItemPrefab.gameObject);
+		Item i = go.GetComponent<Item>();
 		go.name = "Item_"+i.FType.ToString();
+		i.transform.parent = transform;
+
 	}
 
 }
--- dolanburger/Assets/scripts/Burger.cs
+++ dolanburger/Assets/scripts/Burger.cs
@@ -10,65 +10,7 @@ public enum FoodType
 	Vege
 };
 
-public class Item : MonoBehaviour
-{
-	
-	public bool HasDiamond;
-	public int Level;
-	public FoodType FType;
-	public bool IsBurned;
-	private Grill _Grill;
 
-	public void Grill(Grill g)
-	{
-		_Grill = g;
-		transform.parent = g.transform;
-		StartCoroutine(Grilling());
-	}
-
-	public void Stop()
-	{
-		_Grill = null;
-		transform.parent = null;
-		StopAllCoroutines();
-	}
-
-	private IEnumerator Grilling()
-	{
-		while(Level < 4)
-		{
-			yield return new WaitForSeconds(GetGrillTime());
-			Level++;
-			Debug.Log(string.Format("Grilled {0} to level {1}", FType.ToString(), Level));
-		}
-		IsBurned = true;
-		_Grill.OnItemBurned(this);
-	}
-
-	private float GetGrillTime()
-	{
-		float t = 0;
-		switch (FType)
-		{
-		case FoodType.Cheese:
-			t = 1;
-			break;
-		case FoodType.Meat:
-			t = 3;
-			break;
-		case FoodType.Tomato:
-			t = 1.5f;
-			break;
-		case FoodType.Vege:
-			t = 2;
-			break;
-		default:
-			break;
-		}
-
-		return t;
-	}
-}
 
 
 
