d31f090b2d71c666086204b22012b98f90ad8716
Karol Sobiesiak <karol.sobiesiak@unit9.com>
2015-05-15 23:12:25 +0200

    Duck queue.

--- dolanburger/Assets/scripts/DuckQueue.cs
+++ dolanburger/Assets/scripts/DuckQueue.cs
@@ -1,23 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class DuckQueue : MonoBehaviour {
 
     int queueLimit = 3;
     int currNrOfDucks = 0;
 
+    float minDuckSpawnDelay = 5f;
+    float nextDuckSpawnTime = 0;
+
+    // place on the table, duck
+    Dictionary<int, Duck> duckList;
+    bool lastTimeCheckedWasFull = false;
+
 	void Start ()
     {
-
+        nextDuckSpawnTime = Time.realtimeSinceStartup + minDuckSpawnDelay;
 	}
 	
 	void Update ()
     {
-        
+        if (IsReadyToSpawn())
+            SpawnDuck();
 	}
 
-    void AddDuck()
+    void SpawnDuck()
     {
+        int freePlace = GetFreePlace();
+        duckList[freePlace] = new Duck();
+    }
 
+    int GetFreePlace()
+    {
+        for (int i = 0; i < queueLimit; i++)
+        {
+            if (duckList[i] == null)
+                return i;
+        }
+        return 0;
+    }
+
+    bool IsReadyToSpawn()
+    {
+        if (currNrOfDucks < queueLimit)
+        {
+            if (lastTimeCheckedWasFull) // to prevent duck adding to queue immediately after one leave
+            {
+                nextDuckSpawnTime += minDuckSpawnDelay;
+                lastTimeCheckedWasFull = false;
+            }
+            else if (Time.realtimeSinceStartup >= nextDuckSpawnTime)
+                return true;
+        }
+        else
+            lastTimeCheckedWasFull = true;
+        return false;
     }
 }
