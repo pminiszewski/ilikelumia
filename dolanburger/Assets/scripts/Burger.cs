cf332c3116a92fbbb52393c6c51b7d63d414481b
Pawel Miniszewski <venetian5@gmail.com>
2015-05-15 22:00:01 +0200

    - burger, items

--- dolanburger/Assets/scripts/Burger.cs
+++ dolanburger/Assets/scripts/Burger.cs
@@ -1,8 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+
+public enum FoodType
+{
+	Meat,
+	Tomato,
+	Cheese,
+	Vege
+};
+
+public class Item 
+{
+	
+	public bool HasDiamond;
+	public int Level;
+	public FoodType Type;
+}
 
 public class Burger : MonoBehaviour 
 {
+	public List<Item> Items = new List<Item>();
 
 	// Use this for initialization
 	void Start () {
