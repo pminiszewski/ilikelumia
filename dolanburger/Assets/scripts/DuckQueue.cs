09a50d806c95bdc34bf24f38631935265ed18d7f
Karol Sobiesiak <karol.sobiesiak@unit9.com>
2015-05-15 22:49:17 +0200

    Duck queue stub.

---
+++ dolanburger/Assets/scripts/DuckQueue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckQueue : MonoBehaviour {
+
+    int queueLimit = 3;
+    int currNrOfDucks = 0;
+
+	void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+        
+	}
+
+    void AddDuck()
+    {
+
+    }
+}
