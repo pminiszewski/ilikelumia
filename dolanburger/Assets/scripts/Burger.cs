a5f74b1c3d18a6d74653266d57d6120d452f5beb
Pawel Miniszewski <venetian5@gmail.com>
2015-05-15 21:52:00 +0200

    init

---
+++ dolanburger/Assets/scripts/Burger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Burger : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
