9d41cba94341f2d096005e1addd591b39788bb96
Pawel Miniszewski <venetian5@gmail.com>
2015-05-16 00:13:20 +0200

    - dodane zrodla zarcia

---
+++ dolanburger/Assets/scripts/Bucket.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bucket : MonoBehaviour 
+{
+
+	public FoodType BucketType;
+
+
+	public void OnSpawnItem()
+	{
+		GameObject go = new GameObject();
+
+		Item i = go.AddComponent<Item>();
+		i.FType = BucketType;
+		go.name = "Item_"+i.FType.ToString();
+	}
+
+}
