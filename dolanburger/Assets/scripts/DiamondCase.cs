0149c44a0fc824ec3fe1ef783e8aea8964216fec
sapphx <sapphx@gmail.com>
2015-05-16 06:42:04 +0200

    missing script

--- dolanburger/Assets/scripts/DiamondCase.cs
+++ dolanburger/Assets/scripts/DiamondCase.cs
@@ -3,26 +3,47 @@ using System.Collections;
 
 public class DiamondCase : MonoBehaviour {
 
-	bool isOpen = false;
-	float timeToOpen;
+	static public bool isOpen = false;
+	public float timeToOpen = 1;
+	public float time = 0;
 
-	//Ga
+	bool opening = false;
+
+	public GameObject finalPosiotionObj;
+
+	Vector3 finalPosition;
+	Vector3 startingPos;
 
 	public void FlipFlopCaseDoor()
 	{
-		if (isOpen == false)
-		{
-
-		}
+		opening = true;
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		startingPos = this.transform.position;
+		finalPosition = finalPosiotionObj.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (opening == false) {
+			return;
+		}
+
+		if (time >= timeToOpen) {
+			opening = false;
+			isOpen = !isOpen;
+			time = 0;
+			return;
+		}
+
+		time += Time.deltaTime;
+		if (isOpen == true) {
+			transform.position = finalPosition - (finalPosition - startingPos) * time / timeToOpen;
+		} else {
+			transform.position = (finalPosition - startingPos) * time / timeToOpen + startingPos;
+		}
+
 	}
 }
