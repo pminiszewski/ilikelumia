aea6ea05262d15062e87c5b5dcc90ed611c299da
sapphx <sapphx@gmail.com>
2015-05-16 03:42:33 +0200

    Drag fixes

---
+++ dolanburger/Assets/scripts/DiamondCase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiamondCase : MonoBehaviour {
+
+	bool isOpen = false;
+	float timeToOpen;
+
+	//Ga
+
+	public void FlipFlopCaseDoor()
+	{
+		if (isOpen == false)
+		{
+
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
