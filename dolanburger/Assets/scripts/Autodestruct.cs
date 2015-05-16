46fdb1f366026565447509227de1fc59729346d0
sapphx <sapphx@gmail.com>
2015-05-16 10:03:54 +0200

    Autodestruct

---
+++ dolanburger/Assets/scripts/Autodestruct.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Autodestruct : MonoBehaviour {
+
+	public float timeToAutodestruct = 4;
+	float time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time >= timeToAutodestruct) {
+			Destroy(this.transform.gameObject);
+		}
+	}
+}
